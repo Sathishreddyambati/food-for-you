3987ce2638e6a5136fa18674665cdaeb9905fe1c
Supreme Hero Sathi <122785736+Sathishreddyambati@users.noreply.github.com>
2025-07-07 23:32:00 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyCQd4MFvO1q-6Rtr9GrvyyIoO3VF5ibLK0",
   authDomain: "foodforyou-52955.firebaseapp.com",
+  databaseURL: "https://foodforyou-52955-default-rtdb.firebaseio.com",
   projectId: "foodforyou-52955",
   storageBucket: "foodforyou-52955.appspot.com",
   messagingSenderId: "992651702910",
   appId: "1:992651702910:web:5cdebc9f90e90701371e2f"
 };
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-window.firebase = { app, database };// Firebase setup script
+firebase.initializeApp(firebaseConfig);
